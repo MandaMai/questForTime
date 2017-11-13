8f69c4df6528f76f954832bf5dd563cd948c29e8
MandaMai <a.maifeld@hotmail.com>
2017-11-13 10:37:21 -0800

    update files since last work done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { QuestsComponent } from './quests/quests.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    QuestsComponent,
+    TasksComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
